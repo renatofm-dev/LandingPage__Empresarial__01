df4bf9dfa0baf3f0e0f6f4a299f55c2f3eab74c4
Renato F. Martins <rmartins6694@gmail.com>
2023-04-01 00:39:22 -0300

    first commit

---
+++ src/components/especialista/index.jsx
@@ -0,0 +1,31 @@
+import Styles from "./especialista.module.scss";
+import Button from "../button/";
+import { ScrollTo } from "../../utils/scrollTo";
+
+const Especialista = () => {
+  return (
+    <div id="especialista" className={Styles.container}>
+
+      <div className={Styles.text}>
+        <h1>Dr. Jacob Miguel Machado</h1>
+        <p>
+          Advogado e professor especializado em Planejamento Patrimonial,
+          Holdings Familiares, Direito Empresarial e Processo Civil, com
+          vasta experiência em casos complexos envolvendo empresas e grandes famílias.
+            {/* Professor e advogado altamente qualificado e especializado em 
+            Planejamento Patrimonial, Holdings Familiares, 
+            Direito Empresarial e Processo Civil. 
+            Ele possui vasta experiência profissional em diversas causas complexas envolvendo grandes famílias e empresas. */}
+        </p>
+        <Button
+          title="Conheça nosso Especialista"
+          kind="secundary"
+          onClick={() => ScrollTo("contato")}
+        />
+      </div>
+
+    </div>
+  );
+};
+
+export default Especialista;
